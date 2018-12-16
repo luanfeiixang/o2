79607ea2dd923b9e7c6c13da91f2922fecd56750
ergouzi <ergouzi@qq.com>
2018-12-16 19:48:20 +0800

    添加工程到本地仓库

---
+++ src/com/itheima/Git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("二狗子你好啊!!!");
+        System.out.println("二狗子你好啊!!!");
+        System.out.println("二狗子你好啊!!!");
+        System.out.println("二狗子你好啊!!!");
+    }
+}
